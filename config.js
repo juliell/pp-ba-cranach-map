4a93de888139dc6c6c8bcf94e2706f8a856b550c
juliell <julia_elisabeth.voell@smail.th-koeln.de>
2021-01-27 14:33:36 +0100

    updates json cda file, important for images

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 const env = require('dotenv').config();
 
 module.exports = {
-  paintingsUrl: 'https://raw.githubusercontent.com/lucascranach/importer/master/docs/20200911/cda-paintings-v2.de.json',
+  paintingsUrl: 'https://raw.githubusercontent.com/lucascranach/importer/master/docs/20201209/cda-paintings-v2.de.json',
   geocodeUrl: 'https://geocode.search.hereapi.com/v1/geocode',
   geocodeApiKey: env.parsed.API_KEY,
 };
