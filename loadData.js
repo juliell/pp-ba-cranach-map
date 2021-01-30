da030ceb081dc71f5549e65204bdbe7b3ca5b865
juliell <julia_elisabeth.voell@smail.th-koeln.de>
2021-01-30 22:24:56 +0100

    add new functions mapbounds, change card style search for countrys first attempt datedFilter

--- loadData.js
+++ loadData.js
@@ -89,6 +89,37 @@ function addClusterCount() {
   });
 }
 
+function setMapBounds(filteredGeoJSON) {
+  const latLngCoordinates = filteredGeoJSON.features.map((point) => point.geometry.coordinates);
+  let mostWestPoint = [180, 0];
+  let mostEastPoint = [-180, 0];
+  let mostNorthPoint = [0, -90];
+  let mostSouthPoint = [0, 90];
+
+  latLngCoordinates.forEach((point) => {
+    if (mostWestPoint[0] > point[0]) {
+      mostWestPoint = point;
+    }
+    if (mostEastPoint[0] < point[0]) {
+      mostEastPoint = point;
+    }
+    if (mostNorthPoint[1] < point[1]) {
+      mostNorthPoint = point;
+    }
+    if (mostSouthPoint[1] > point[1]) {
+      mostSouthPoint = point;
+    }
+  });
+
+  // // eslint-disable-next-line new-cap
+  lngLatBounds = new mapboxgl.LngLatBounds([mostWestPoint[0], mostSouthPoint[1]], [mostEastPoint[0], mostNorthPoint[1]]);
+
+  map.fitBounds(lngLatBounds, {
+    padding: {
+      top: 60, bottom: 60, left: 500, right: 60,
+    },
+  });
+}
 function addData(filteredGeoJSON = null) {
   map.on('load', () => {
     console.log('map load');
@@ -100,6 +131,9 @@ function addData(filteredGeoJSON = null) {
       // clusterMaxZoom: 6,
     });
 
+    if (filteredGeoJSON) {
+      setMapBounds(filteredGeoJSON);
+    }
     addClusterLayer();
     addUnclusteredLayer();
     addClusterCount();
@@ -117,35 +151,58 @@ async function loadData() {
 }
 
 function renderCard(searchResult, s) {
-  const imageURL = searchResult.image !== '' ? searchResult.image : 'No-image-available.png';
+  console.log(typeof searchResult.properties.dating);
+  const paintingURL = searchResult.properties.image !== '' ? searchResult.properties.image : 'No-image-available.png';
+  const paintingTitle = searchResult.properties.titles.length > 41 ? `${searchResult.properties.titles.substring(0, 40)}...` : searchResult.properties.titles;
+  const paintingCoordinates = JSON.stringify(searchResult.geometry.coordinates);
   if (s === 1) {
-    return `<div class="card mb-3" style="width: 100%; height: 200px">
+    return `<div class="card mb-3" style="width: 100%; height: 200px; margin-bottom: 8px !important;">
   <div class="row g-0 m-0" style="height: inherit">
     <div class="col-md-5 img p-0" style="height: inherit;">
-      <img class="center-block" src="${imageURL}" alt="kein Bild verfügbar">
+      <img class="center-block" src="${paintingURL}" alt="kein Bild verfügbar">
     </div>
     <div class="col-md-7 p-0" style="height: inherit">
       <div class="card-body p-2" style="height: inherit">
-        <h5 class="card-title search">${searchResult.titles}</h5>
-        <p class="card-text search" style="
-        margin-bottom: 5px;">${searchResult.dated}<br>${searchResult.repository}<br>${searchResult.location}, ${searchResult.country}</p>
-        <p class="card-text search"><small class="text-muted">
-        <a target="_blank" href="http://www.lucascranach.org/${searchResult.inventoryNumber}">mehr Infos</a></small></p>
+        <h5 class="card-title search" style="font-size: 18px;"
+        data-toggle="tooltip" data-placement="top" title="${searchResult.properties.titles}">${paintingTitle}</h5>
+        <p class="card-text search" style="margin-bottom: 5px; font-size: 12px;">
+        ${searchResult.properties.dated}<br>
+        ${searchResult.properties.repository}<br>
+        ${searchResult.properties.location}, ${searchResult.properties.country}</p>
+        <div class="row">
+          <div class="col-md-2">
+          <button class="btn p-0" type="button">
+            <a target="_blank" href="http://www.lucascranach.org/${searchResult.properties.inventoryNumber}">  
+            <i class="bi bi-info-circle-fill paintingIcon"></i></a>
+          </button>
+            </div>
+          <div class="col-md-2" style="padding-left: 5px;">
+            <button class="btn p-0 paintingMarker" type="button" data-location="${paintingCoordinates}">
+            <i class="bi bi-geo-alt-fill paintingIcon"></i></a>
+            </button>
+           </div>
+        </div>
       </div>
     </div>
   </div>
 </div>`;
   } return `<div class="card popupCard">
-  <div class="row g-0 m-0" style="height: 150px;width: 290px;">
+  <div class="row g-0 m-0" style="height: 150px; width: 280px; margin-bottom: 8px !important;">
     <div class="col-md-5 img p-0" style="height: 100%;">
-      <img class="center-block" src="${imageURL}" alt="kein Bild verfügbar">
+      <img class="center-block" src="${paintingURL}" alt="kein Bild verfügbar">
     </div>
     <div class="col-md-7 p-0" style="height: 100%;">
       <div class="card-body p-0" style="margin-top: 5px">
-        <h6 class="card-title popupTitle">${searchResult.titles}</h6>
-        <p class="card-text popupText">${searchResult.dated}<br>${searchResult.repository}<br>${searchResult.location}, ${searchResult.country}</p>
-        <p class="card-text popupText"><small class="text-muted">
-        <a target="_blank" href="http://www.lucascranach.org/${searchResult.inventoryNumber}">mehr Infos</a></small></p>
+        <h5 class="card-title popupTitle" data-toggle="tooltip" data-placement="top" title="${searchResult.properties.titles}">${paintingTitle}</h5>
+        <p class="card-text popupText">${searchResult.properties.dated}<br>
+        ${searchResult.properties.repository}<br>
+        ${searchResult.properties.location}, ${searchResult.properties.country}</p>
+        <div class="col-md-2 p-0">
+          <button class="btn p-0" type="button">
+            <a target="_blank" href="http://www.lucascranach.org/${searchResult.properties.inventoryNumber}">  
+            <i class="bi bi-info-circle-fill paintingIcon"></i></a>
+          </button>
+        </div>
       </div>
     </div>
   </div>
@@ -156,18 +213,71 @@ function search() {
   if ($('#searchInput').val().length > 2) {
     const searchString = searchInput.value.toLowerCase();
     const filteredResult = { ...paintingsGeoJSON };
-    filteredResult.features = paintingsGeoJSON.features.filter((elem) => elem.properties.titles.toLowerCase().includes(searchString));
-
+    filteredResult.features = paintingsGeoJSON.features.filter((elem) => {
+      return elem.properties.titles.toLowerCase().includes(searchString)
+        || elem.properties.location.toLowerCase().includes(searchString)
+        || elem.properties.repository.toLowerCase().includes(searchString);
+    });
     let resultListHTML = '';
     filteredResult.features.forEach((result) => {
-      resultListHTML += renderCard(result.properties, s = 1);
+      resultListHTML += renderCard(result, s = 1);
     });
-    $('#searchResults').html(resultListHTML);
-    initMap();
-    clusters();
-    addData(filteredResult);
+
+    if (resultListHTML === '') {
+      $('.searchResults').html(`<div class="container mp-0">
+      <p>Die Suche nach '${searchString}' ergab kein Ergebnis.</p></div>`);
+      $('#countResults').html(filteredResult.features.length);
+    } else {
+      $('.searchResults').html(resultListHTML);
+      $('#countResults').html(filteredResult.features.length);
+      $('.resultContainer').attr('style', 'display: block !important');
+      initMap();
+      clusters();
+      addData(filteredResult);
+      
+
+      $('.paintingMarker').click((e) => {
+        markerLocation = JSON.parse(e.currentTarget.dataset.location);
+        map.flyTo({
+          center: markerLocation,
+          zoom: 9,
+          speed: 0.6,
+        });
+      });
+    }
   }
 }
+
+// function datedFilter(year0, year1) {
+//   const filteredResult = { ...paintingsGeoJSON };
+//   //filteredResult.features = paintingsGeoJSON.features.filter((elem) => elem.properties.dated.includes(year0 || year1));
+
+//   let list = '';
+//   paintingsGeoJSON.features.forEach((item) => {
+//     if (year0 <= item.properties.dating[1] && year1 <= item.properties.dating[2]) list += item;
+//   });
+//   console.log(list);
+
+
+//   // let resultListHTML = '';
+//   // filteredResult.features.forEach((result) => {
+//   //   resultListHTML += renderCard(result, s = 1);
+//   // });
+
+//   // $('.searchResults').html(resultListHTML);
+//   initMap();
+//   clusters();
+//   addData(filteredResult);
+
+//   $('.paintingMarker').click((e) => {
+//     markerLocation = JSON.parse(e.currentTarget.dataset.location);
+//     map.flyTo({
+//       center: markerLocation,
+//       zoom: 9,
+//       speed: 0.6,
+//     });
+//   });
+// }
 
 function addPopupCluster(e, features, clusterId, clusterSource) {
   const pointCount = features[0].properties.point_count;
@@ -178,7 +288,7 @@ function addPopupCluster(e, features, clusterId, clusterSource) {
       clusterSource.getClusterLeaves(clusterId, pointCount, 0, (error, leavesFeatures) => {
         let popupText = `<h5 Class="popupCity">${leavesFeatures[0].properties.location}</h5>`;
         leavesFeatures.forEach((item) => {
-          popupText += renderCard(item.properties, s = 0);
+          popupText += renderCard(item, s = 0);
         });
 
         new mapboxgl.Popup({
@@ -199,16 +309,22 @@ function clickClusters() {
     });
     const clusterId = features[0].properties.cluster_id;
     const clusterSource = map.getSource('paintings');
-
     map.getSource('paintings').getClusterExpansionZoom(
       clusterId,
       (err, zoom) => {
-        if (err) return;
-        map.easeTo({
+        map.flyTo({
           center: features[0].geometry.coordinates,
-          zoom: (zoom === 18 ? 6 : zoom),
+          zoom: zoom,
+          speed: 0.6,
         });
       },
+      // (err, zoom) => {
+      //   if (err) return;
+      //   map.easeTo({
+      //     center: features[0].geometry.coordinates,
+      //     zoom: (zoom === 18 ? 6 : zoom),
+      //   });
+      // },
     );
     addPopupCluster(e, features, clusterId, clusterSource);
   });
@@ -217,29 +333,21 @@ function clickClusters() {
 function clickUnclusteredPoint() {
   map.on('click', 'unclustered-point', (e) => {
     const coordinates = e.features[0].geometry.coordinates.slice();
-    // console.log(e.features[0].properties);
-    // const clusterId = e.features[0].properties.cluster_id;
-    // map.getSource('paintings').getClusterExpansionZoom(
-    //   clusterId,
-    //   (err, zoom) => {
-    //     if (err) return;
-    //     map.easeTo({
-    //       center: features[0].geometry.coordinates,
-    //       zoom: (zoom === 18 ? 6 : zoom),
-    //     });
-    //   },
-    // );
 
-    // Ensure that if the map is zoomed out such that
-    // multiple copies of the feature are visible, the
-    // popup appears over the copy being pointed to.
+    currentZoomLevel = map.getZoom();
+    console.log(currentZoomLevel);
+    map.flyTo({
+      center: e.features[0].geometry.coordinates,
+      zoom: (currentZoomLevel < 5 ? 5 : currentZoomLevel),
+      speed: 0.6,
+    });
     while (Math.abs(e.lngLat.lng - coordinates[0]) > 180) {
       coordinates[0] += e.lngLat.lng > coordinates[0] ? 360 : -360;
     }
 
     new mapboxgl.Popup()
       .setLngLat(coordinates)
-      .setHTML(`<h5 class="popupCity">${e.features[0].properties.location}</h5>${renderCard(e.features[0].properties)}`)
+      .setHTML(`<h5 class="popupCity">${e.features[0].properties.location}</h5>${renderCard(e.features[0])}`)
       .addTo(map);
   });
 }
@@ -248,26 +356,22 @@ function clusters() {
   clickClusters();
   clickUnclusteredPoint();
 
-  // const popup = new mapboxgl.Popup({
-  //   className: 'popups',
-  //   closeButton: false,
-  //   closeOnClick: false,
-  // });
-
-  map.on('mouseenter', 'clusters', (e) => {
+  map.on('mouseenter', 'clusters', () => {
     map.getCanvas().style.cursor = 'pointer';
-
-    // hover Popup
-    // const coordinates = e.features[0].geometry.coordinates.slice();
-    // const { location } = e.features[0].properties;
-    // console.log({location});
-    // var locations = e.features[0].properties.description;
-    // popup.setLngLat(coordinates).setHTML('Test').addTo(map);
   });
+
+  map.on('mouseenter', 'unclustered-point', () => {
+    map.getCanvas().style.cursor = 'pointer';
+  });
+
   map.on('mouseleave', 'clusters', () => {
     map.getCanvas().style.cursor = '';
-    // popup.remove();
   });
+
+  map.on('mouseleave', 'unclustered-point', () => {
+    map.getCanvas().style.cursor = '';
+  });
+
 }
 
 $(document).ready(async () => {
@@ -277,10 +381,45 @@ $(document).ready(async () => {
   search();
 
   $('#searchInput').on('keyup', (e) => {
-    if (e.which === 13) search();
+    if ($('#searchInput').val().length > 0) {
+      $('#resetSearch').attr('style', 'display: inline !important');
+    } else $('#resetSearch').attr('style', 'display: none !important');
+    if (e.which === 13) {
+      search();
+    }
   });
 
-  $('#searchButton').click(() => {
+  $('#resetSearch').click(() => {
+    $('#resetSearch').attr('style', 'display: none !important');
+    $('#searchInput').val('');
+    $('.searchResults').html('');
+    $('.resultContainer').attr('style', 'display: none !important');
+    $('#countResults').html('');
+    initMap();
+    clusters();
+    addData();
+  });
+
+  $('.searchbarButton').click(() => {
     search();
   });
+
+  $('.yearSearchButton').click(() => {
+    const year0 = $('#year0Input').val();
+    const year1 = $('#year1Input').val();
+    console.log(year0, year1);
+    if (year1 > year0) {
+      datedFilter(year0, year1);
+    }
+  });
+
+  // $('.hideSearchResults').click(() => {
+  //   $('.showSearchResults').show();
+  //   $('.hidesearchResults').hide();
+  // });
+
+  // $('.showSearchResults').click(() => {
+  //   $('.showSearchResults').hide();
+  //   $('.hidesearchResults').show();
+  // });
 });
