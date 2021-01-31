af3a8e3fb2a498b2f05ebcefc26949e29567bd0b
juliell <julia_elisabeth.voell@smail.th-koeln.de>
2021-01-31 17:24:21 +0100

    change folderstructure

--- backend/server.js
+++ backend/server.js
@@ -1,17 +1,18 @@
 const express = require('express');
+const path = require('path');
 
 const server = express();
 
-server.use(express.static(__dirname));
+server.use(express.static(path.join(`${__dirname}/../frontend`)));
 
 const port = 3000;
 
 server.get('/', (req, res) => {
-  res.sendFile(`${__dirname}/index.html`);
+  res.sendFile(path.join(`${__dirname}/../frontend/index.html`));
 });
 
 server.get('/data', (req, res) => {
-  res.sendFile(`${__dirname}/data.geojson`);
+  res.sendFile(path.join(`${__dirname}/data.geojson`));
 });
 
 server.listen(port, () => {
